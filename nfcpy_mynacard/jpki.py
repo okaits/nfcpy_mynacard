2f2217758efeabfe824af448c2e263ee56352f95
okaits <okaits@okaits7534.net>
2025-08-25 04:35:35 +0900

    JPKI署名機能を追加

--- nfcpy_mynacard/jpki.py
+++ nfcpy_mynacard/jpki.py
@@ -1,5 +1,7 @@
 import typing
+import hashlib
 import nfc
+import asn1
 import nfcpy_mynacard.login
 import nfcpy_mynacard.card
 
@@ -16,6 +18,29 @@ class UserProofCert:
         cert_der = nfcpy_mynacard.card.communicate(tag, bytes.fromhex("00 B0 00 00 00 00 00"))
 
         return cert_der
+    
+    @staticmethod
+    def sign_data(tag: nfc.tag.Tag, password: int, data: bytes) -> bytes:
+        """ データを署名し、bytesで返却する。（要認証・JPKI利用者認証用電子証明書） """
+        nfcpy_mynacard.card.select_ap(tag, nfcpy_mynacard.card.AP_DF["JPKI"])
+        nfcpy_mynacard.login.JPKI.user_proof_cert(tag, password)
+        nfcpy_mynacard.card.select_ef(tag, bytes.fromhex("00 17"))
+
+        # 署名の命令の前に、DigestInfo（ASN.1）を発行する
+        digestinfo_encoder = asn1.Encoder()
+        digestinfo_encoder.start()
+        digestinfo_encoder.enter(asn1.Numbers.Sequence)
+        digestinfo_encoder.enter(asn1.Numbers.Sequence)
+        digestinfo_encoder.write("2.16.840.1.101.3.4.2.1", asn1.Numbers.ObjectIdentifier)
+        digestinfo_encoder.write(None, asn1.Numbers.Null)
+        digestinfo_encoder.leave()
+        digestinfo_encoder.write(hashlib.sha256(data).digest(), asn1.Numbers.OctetString)
+        digestinfo_encoder.leave()
+        digestinfo = digestinfo_encoder.output()
+
+        # 満を持して署名を命令
+        sign_result = nfcpy_mynacard.card.communicate(tag, bytes.fromhex("80 2A 00 80") + len(digestinfo).to_bytes() + digestinfo + (0).to_bytes())
+        return sign_result
 
 class SignatureCert:
     """ 署名用電子証明書 """
@@ -31,3 +56,26 @@ class SignatureCert:
         cert_der = nfcpy_mynacard.card.communicate(tag, bytes.fromhex("00 B0 00 00 00 00 00"))
 
         return cert_der
+    
+    @staticmethod
+    def sign_data(tag: nfc.tag.Tag, password: str, data: bytes) -> bytes:
+        """ データを署名し、bytesで返却する。（要認証・JPKI署名用電子証明書） """
+        nfcpy_mynacard.card.select_ap(tag, nfcpy_mynacard.card.AP_DF["JPKI"])
+        nfcpy_mynacard.login.JPKI.signature_cert(tag, password)
+        nfcpy_mynacard.card.select_ef(tag, bytes.fromhex("00 1A"))
+
+        # 署名の命令の前に、DigestInfo（ASN.1）を発行する
+        digestinfo_encoder = asn1.Encoder()
+        digestinfo_encoder.start()
+        digestinfo_encoder.enter(asn1.Numbers.Sequence)
+        digestinfo_encoder.enter(asn1.Numbers.Sequence)
+        digestinfo_encoder.write("2.16.840.1.101.3.4.2.1", asn1.Numbers.ObjectIdentifier)
+        digestinfo_encoder.write(None, asn1.Numbers.Null)
+        digestinfo_encoder.leave()
+        digestinfo_encoder.write(hashlib.sha256(data).digest(), asn1.Numbers.OctetString)
+        digestinfo_encoder.leave()
+        digestinfo = digestinfo_encoder.output()
+
+        # 満を持して署名を命令
+        sign_result = nfcpy_mynacard.card.communicate(tag, bytes.fromhex("80 2A 00 80") + len(digestinfo).to_bytes() + digestinfo + (0).to_bytes())
+        return sign_result
